24cdece2a8df74873843f1252bffe0132ebd980e
BoatOD <saoraza1234@gmail.com>
2022-11-25 21:48:42 +0700

    docker setup with Dockerfile and a build script

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from app import app
-
-
-
+ 
 @app.route('/')
 def home():
     return "Flask says 'Hello world!'"
+
+@app.route('/phonebook')
+def index():
+    return app.send_static_file('phonebook.html')
